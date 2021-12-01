e027278db9abb4c8d9e01cdbe2ecbe87818478b5
Olavo Borges <olavoborges@gmail.com>
2021-12-01 20:00:45 -0300

    guardiantest

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 from ibm_appconfiguration import AppConfiguration, Feature, Property, ConfigurationType
 app_configuration_client = AppConfiguration.get_instance()
 app_configuration_client.init(region='us-south',
-               guid='2863271a-cfa1-44ff-8cd0-89b21c61cc0a',
-               apikey='PEsQkYW90e520ZAwpUnvYCDn3GGP8HQ7nHdJILyw1bB4')
+               guid='',
+               apikey='')
 
 ## Initialize configurations 
 app_configuration_client.set_context(collection_id='studycase4-collection', environment_id='dev')
